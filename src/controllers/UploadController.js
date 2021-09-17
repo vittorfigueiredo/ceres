eac607b4a4b0814478428c9d2b143466b138d81e
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-09-17 10:18:18 -0300

    adiciona validações

--- src/controllers/UploadController.js
+++ src/controllers/UploadController.js
@@ -1,10 +1,36 @@
 const { s3Client } = require("../database/S3Client");
 const { PutObjectCommand } = require("@aws-sdk/client-s3");
 const { makeFileName } = require("../functions/MakeFileName");
+const { verifyBase64 } = require("../functions/VerifyBase64");
 
 class UploadController {
   async send(request, response) {
     const { base64 } = request.body;
+
+    if (base64 === undefined) {
+      return response.json({
+        success: false,
+        message: "O parâmetro base64 é obrigatório!"
+      });
+    }
+
+    if (base64.length === 0) {
+      return response.json({
+        success: false,
+        message: "O parâmetro base64 não pode ser vazio!"
+      });
+    }
+
+    const isBase64 = verifyBase64(base64);
+
+    if (!isBase64) {
+      return response.json({
+        success: false,
+        message: "Informe um base64 válido!"
+      })
+    }
+
+  console.log("cheguei aqui");
 
     const params = {
       Bucket: process.env.AWS_BUCKET,
@@ -17,6 +43,13 @@ class UploadController {
     // Create an object and upload it to rhe Amazon S3 bucket.
     try {
       const results = await s3Client.send(new PutObjectCommand(params));
+
+      if (results.$metadata.httpStatusCode !== 200) {
+        return response.json({
+          success: false,
+          message: "Erro inesperado!"
+        });
+      }
 
       return response.json({
         success: true,
