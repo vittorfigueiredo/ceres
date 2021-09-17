283acfe9bdae1f514f363b00be2c4067e6e452f3
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-09-17 11:57:34 -0300

    ajuste para usar o region a partir do env

--- src/database/S3Client.js
+++ src/database/S3Client.js
@@ -1,6 +1,6 @@
 const { S3Client } = require("@aws-sdk/client-s3");
 
-const region = "us-east-2";
+const region = process.env.AWS_REGION;
 const s3Client = new S3Client({ region });
 
 module.exports = { s3Client };
