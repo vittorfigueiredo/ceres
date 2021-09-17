e3a419f05098bde369055f304d0b4ac237d50b91
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-09-17 11:56:07 -0300

    importa cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const { router } = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 app.listen(process.env.PORT || 3333, () => {
