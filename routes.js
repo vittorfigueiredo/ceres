79acfb1a5b5d9d25034e1c7ca9c30c1c632f9a1b
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-09-17 11:23:51 -0300

    mensagem rota get

--- routes.js
+++ routes.js
@@ -6,7 +6,10 @@ const router = express.Router()
 const uploadController = new UploadController();
 
 router.get("/", (request, response) => {
-  return response.json({ node: process.version});
+  return response.json({
+    Luke: "Mas eu não acredito!",
+    Yoda: "É por isso que você fracassa!"
+  });
 });
 
 router.post("/upload", uploadController.send);
