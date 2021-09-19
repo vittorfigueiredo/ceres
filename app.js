f448891653bb66b9c0c6f77c4dc90e324c668ec2
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-09-18 23:28:50 -0300

    ajuste na importação do cors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const { router } = require("./routes");
-const cors = require("cors");
 
 const app = express();
 
