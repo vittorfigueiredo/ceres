f38950f31a8dcf6fa877efed93888e29edcd884e
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-09-18 23:22:22 -0300

    adiciona validações para o user e password

--- src/controllers/AuthenticationController.js
+++ src/controllers/AuthenticationController.js
@@ -1,23 +1,54 @@
 const jwt = require("jsonwebtoken");
 
 class AuthenticationController {
-  async auth(request,response) {
+  async auth(request, response) {
     const { user, password } = request.body;
 
-    if (user === process.env.AUTH_USER && password === process.env.AUTH_PASSWORD) {
+    if (user === undefined) {
+      return response.status(401).json({
+        success: false,
+        message: "O parâmetro user é obrigatório!",
+      });
+    }
+
+    if (user.length === 0) {
+      return response.status(401).json({
+        success: false,
+        message: "O parâmetro user não pode ser vazio!",
+      });
+    }
+
+    if (password === undefined) {
+      return response.status(401).json({
+        success: false,
+        message: "O parâmetro password é obrigatório!",
+      });
+    }
+
+    if (password.length === 0) {
+      return response.status(401).json({
+        success: false,
+        message: "O parâmetro password não pode ser vazio!",
+      });
+    }
+
+    if (
+      user === process.env.AUTH_USER &&
+      password === process.env.AUTH_PASSWORD
+    ) {
       const id = process.AUTH_ID;
 
       const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: "1d"
+        expiresIn: "1d",
       });
 
       return response.status(201).json({
         auth: true,
-        token: token
+        token: token,
       });
     }
     return response.status(401).json({
-      message: "Login inválido!"
+      message: "Login inválido!",
     });
   }
 }
