b8839f393ea9ab4f291d9e066fe80fd6e5af999f
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-09-20 09:28:23 -0300

    adiciona verificação de chave secreta e deixa de usar token jwt

--- src/controllers/UploadController.js
+++ src/controllers/UploadController.js
@@ -5,57 +5,77 @@ const { verifyBase64 } = require("../functions/VerifyBase64");
 
 class UploadController {
   async send(request, response) {
-    const { base64 } = request.body;
+    const { base64, secret } = request.body;
 
-    if (base64 === undefined) {
+    if (secret === undefined) {
       return response.status(400).json({
         success: false,
-        message: "O parâmetro base64 é obrigatório!"
+        message: "O parâmetro secret é obrigatório!",
       });
     }
 
-    if (base64.length === 0) {
+    if (secret.length === 0) {
       return response.status(400).json({
         success: false,
-        message: "O parâmetro base64 não pode ser vazio!"
+        message: "O parâmetro secret não pode ser vazio!",
       });
     }
 
-    const isBase64 = verifyBase64(base64);
-
-    if (!isBase64) {
-      return response.status(400).json({
-        success: false,
-        message: "Informe um base64 válido!"
-      })
-    }
-
-    const params = {
-      Bucket: process.env.AWS_BUCKET,
-      Key: makeFileName(32) + ".jpg",
-      Body: Buffer.from(base64, "base64"),
-      ContentEncoding: "base64",
-      ContentType: "image/jpeg",
-    };
-
-    // Create an object and upload it to rhe Amazon S3 bucket.
-    try {
-      const results = await s3Client.send(new PutObjectCommand(params));
-
-      if (results.$metadata.httpStatusCode !== 200) {
-        return response.status(500).json({
+    if (secret === process.env.SECRET_KEY) {
+      if (base64 === undefined) {
+        return response.status(400).json({
           success: false,
-          message: "Erro inesperado!"
+          message: "O parâmetro base64 é obrigatório!",
         });
       }
 
-      return response.status(201).json({
-        success: true,
-        URL: `https://${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${params.Key}`,
-      });
-    } catch (err) {
-      console.log("Error", err);
+      if (base64.length === 0) {
+        return response.status(400).json({
+          success: false,
+          message: "O parâmetro base64 não pode ser vazio!",
+        });
+      }
+
+      const isBase64 = verifyBase64(base64);
+
+      if (!isBase64) {
+        return response.status(400).json({
+          success: false,
+          message: "Informe um base64 válido!",
+        });
+      }
+
+      const params = {
+        Bucket: process.env.AWS_BUCKET,
+        Key: makeFileName(32) + ".jpg",
+        Body: Buffer.from(base64, "base64"),
+        ContentEncoding: "base64",
+        ContentType: "image/jpeg",
+      };
+
+      // Create an object and upload it to rhe Amazon S3 bucket.
+      try {
+        const results = await s3Client.send(new PutObjectCommand(params));
+
+        if (results.$metadata.httpStatusCode !== 200) {
+          return response.status(500).json({
+            success: false,
+            message: "Erro inesperado!",
+          });
+        }
+
+        return response.status(201).json({
+          success: true,
+          URL: `https://${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${params.Key}`,
+        });
+      } catch (err) {
+        console.log("Error", err);
+      }
     }
+    return response.status(401).json({
+      success: false,
+      message: "Unauthorized!",
+    });
   }
 }
 
